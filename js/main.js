9948fa4b24cafaa146fd15e2bda6aa11cd523ad4
khaled404 <khaled.ma805@gmail.com>
2020-07-29 00:36:09 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function () {
+  $(".sideNavButton").on("click", function () {
+    $(".min-content-container").toggleClass("active");
+    $(".side-nav-conatiner").toggleClass("active");
+  });
+});
